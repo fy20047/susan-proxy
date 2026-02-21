cce76555598ecec35a1994733af70e6672896278
fy20047 <fy20001215@gmail.com>
2026-02-21 13:59:37 +0800

    Initial commit: 建立代購系統後端基礎專案

---
+++ src/main/java/com/fy20047/susan/SusanApplication.java
@@ -0,0 +1,13 @@
+package com.fy20047.susan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SusanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SusanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fy20047/susan/SusanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fy20047.susan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SusanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
